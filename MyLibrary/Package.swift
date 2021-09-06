4558178461e304534b27b1d7800109c9a93d6704
olaf-watcha <olaf@watcha.com>
2021-09-06 18:26:08 +0900

    qweqweqwe

--- MyLibrary/Package.swift
+++ MyLibrary/Package.swift
@@ -20,9 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyLibrary",
-            path: "Sources/MyLibrary"),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            resources: [.process("Resources")]),
+        .target(name: "Assets",
+                resources: [.process("Resources")])
     ]
 )
